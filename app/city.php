02269f82695c678d88e6e90b4697bf63fe8ebdeb
live2pro <live2pro@gmail.com>
2017-02-26 01:56:19 +0700

    first commit

---
+++ app/city.php
@@ -0,0 +1,48 @@
+﻿<?php
+$query = $_REQUEST['query'];
+require_once 'D:/search/elasticsearch/elasticsearch/vendor/autoload.php';
+
+use Elasticsearch\ClientBuilder;
+
+ 
+ 
+ //echo "req is".$query;
+//$query="เขต";
+$client = ClientBuilder::create()->build();
+	
+
+$params1 = [
+    'index' => 'music1',
+    'type' => 'song1',
+    'body' => [
+	'_source'=>'suggest',
+	'suggest' => [
+		'song-suggest'=> [
+		
+			'prefix'=>"$query",
+			'completion'=>[
+				'field'=>'suggest']
+			]
+		]
+
+    ]
+];
+
+
+$response = $client->search($params1);
+foreach ($response['suggest']['song-suggest'][0]['options'] as $value) {
+    $result[]=$value['text'];
+	
+}
+//echo json_encode ($result);
+//print_r($response['suggest']['song-suggest'][0]['options'][1]['text']);
+
+echo json_encode($result);
+
+
+	
+	
+	
+	
+	
+	?>
